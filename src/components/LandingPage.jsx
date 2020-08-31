42113cccdf20da6e688e846d304e468ff2edd126
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-31 18:50:20 +0200

    creating and importing logo, creating navbar component, styling login and sign up buttons

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const LandingPage = () => {
   return (
     <>
       <div id="left-pane">
+        <img id="logo" src="./images/logo.png" alt="BidSpace" />
         <button id="rent-button">Rent a space!</button>
       </div>
       <div id="right-pane">
-      <button id="login-button">Login</button>
-      <button id="signup-button">Signup</button>
-      <button id="rentout-button">Rent out a space!</button>
+        <Navbar />
+        <button id="rentout-button">Rent out a space!</button>
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Menu } from "semantic-ui-react";
 
 const Navbar = () => {
   return (
     <div>
-      
+      <Menu secondary id="navbar">
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <button id="login-button">Login</button>
+          </Menu.Item>
+
+          <Menu.Item>
+            <button id="signup-button">Signup</button>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
     </div>
-  )
-}
+  );
+};
 
-
-export default Navbar
+export default Navbar;
