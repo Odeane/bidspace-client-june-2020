20bde192e5c9b2b6d013ecc37da3bf5758371f91
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-31 13:55:53 +0200

    writing initial test that landing page renders and elements appear, creating landing page component and importing in app.jsx

---
+++ cypress/integration/userCanSeeLandingPage.feature.js
@@ -0,0 +1,14 @@
+describe("Visitors can see landing page", () => {
+  before(() = {
+  cy.visit("/")
+  }
+  
+  it("and choose whether to rent out or rent a space", () => {
+    cy.get("#logo").should("be.visible")
+    cy.get("#login-button").should("be.visible")
+    cy.get("#signup-button").should("be.visible")
+    cy.get("hamburger-menu").should("be.visble")
+    cy.get("#rentout-button").should("be.visible")
+    cy.get("#rent-button").should("be.visible")
+  })
+})
