4a336612e63f7a44383928a89b9854bd10212204
viktor_sommar <vsommarbaillot@gmail.com>
2020-09-01 15:07:39 +0200

    Adding implementation code and some redux.

--- src/components/ListingPage.jsx
+++ src/components/ListingPage.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const ListingPage = () => {
+const ListingPage = (props) => {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    getListings();
+  }, []);
+
+  const getListings = async () => {
+    let response = await axios.get(`/listings`);
+    setListings(response.data.articles);
+  };
+
+  let content = <>{listings.map((listing) => (listing = { listing }))}</>;
+
   return (
     <>
       <h1 id="rent-space-title">Rent your space</h1>
+      <div>{content}</div>
     </>
   );
 };
