73cbeb3d7737e5699971e1ccb1caa36e9a1c6b45
viktor_sommar <vsommarbaillot@gmail.com>
2020-09-05 20:13:22 +0200

    Changing up the landing page using semantic instead of pure css

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,30 +1,89 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
-import { connect } from "react-redux"
-import LoginForm from "./LoginForm"
+import { connect } from "react-redux";
+import LoginForm from "./LoginForm";
+import { Container, Header, Button, Icon, Segment, Grid, Image, Divider } from "semantic-ui-react"
 
 const LandingPage = (props) => {
   return (
     <>
-      <div id="left-pane">
-        <img id="logo" src="./images/logo.png" alt="BidSpace" />
-        <Link data-cy="button" id="rent-button" to="/rent-space">Rent a space!</Link>
-      </div>
-      <div id="right-pane">
-        <Navbar />
-        <Link data-cy="button" id="rentout-button" to="/rentout-space">Rent out a space!</Link>
-      </div>
-      {props.renderLoginForm && <LoginForm />}
+    <Segment inverted>
+      <Container text>
+        <Header as="h1" content="BidSpace" inverted />
+        <Header
+          as="h2"
+          content="Find or rent a space that fits you!"
+          inverted
+        />
+        <Button primary size="huge">
+          Read more below!
+        </Button>
+      </Container>
+      </Segment>
+
+      <Segment vertical>
+        <Grid container stackable verticalAlign="middle">
+          <Grid.Row>
+            <Grid.Column width={8}>
+              <Header as="h3">
+                We help you find your space.
+              </Header>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id in molestias similique cumque labore fugiat minus animi, voluptatum maxime, voluptas odio modi consectetur voluptatibus quos laboriosam illo dignissimos mollitia. Impedit?</p>
+            </Grid.Column>
+            <Grid.Column floated="right" width={6}>
+              <Image
+              bordered
+              rounded
+              size="large"
+              src="https://i.redd.it/w3kr4m2fi3111.png"
+              />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column textAlign="center">
+              <Button size="huge">Check out our spaces!</Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Divider horizontal>Or</Divider>
+
+      <Segment vertical>
+        <Grid container stackable verticalAlign="middle">
+        <Grid.Row>
+            <Grid.Column textAlign="center">
+              <Button size="huge">Rent out your space!</Button>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column floated="left" width={6}>
+              <Image
+              bordered
+              rounded
+              size="large"
+              src="https://i.redd.it/w3kr4m2fi3111.png"
+              />
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Header as="h3">
+                We help you find your space.
+              </Header>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id in molestias similique cumque labore fugiat minus animi, voluptatum maxime, voluptas odio modi consectetur voluptatibus quos laboriosam illo dignissimos mollitia. Impedit?</p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
     </>
   );
 };
 
 const mapStateToProps = (state) => {
-  return{
+  return {
     renderLoginForm: state.renderLoginForm,
     authenticated: state.authenticated,
-  }
-}
+  };
+};
 
 export default connect(mapStateToProps)(LandingPage);
