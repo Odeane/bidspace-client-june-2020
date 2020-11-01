e638ebca2aed8f9fbb777421cf77869d7b5c1e71
odeane_croney <odeane_croney@yahoo.com>
2020-11-01 19:08:47 +0100

    rearranging reducers code, now able to combine reducers

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import axios from "axios";
+import apiUrl from './api/baseURL'
 import configureStore from "./state/store/configureStore";
 import { StripeProvider } from "react-stripe-elements";
+
+axios.defaults.baseURL = apiUrl;
 
 const store = configureStore();
 
